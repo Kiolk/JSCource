84181bf29748c2a679cadd1742a5e0242548d7d5
eugene.slizh <Moov2Ej1>
2019-04-04 17:37:52 +0300

    Some solutions

--- src/task3/index.js
+++ src/task3/index.js
@@ -1,3 +1,48 @@
-module.exports.isTimeValide = function(a, b){
-    return false;
-} 
+module.exports.isTimeValide = function(a, b) {
+  return false;
+};
+
+module.exports.addMinutes = function(hours, minutes, additionTime) {
+  return "";
+};
+
+module.exports.isPointInCircle = function(x, y) {
+  const cycleCenterX = 3;
+  const cycleCenterY = 5;
+  const cycleRadius = 4;
+  const length = Math.sqrt(
+    Math.pow(cycleCenterX - x, 2) + Math.pow(cycleCenterY - y, 2)
+  );
+  return length <= cycleRadius;
+};
+
+module.exports.isPointInSquare = function(x, y) {
+
+  return firstLine(x, y) && !secondLine(x, y) && thirdLine(x,y) && !forthLine(x, y);
+};
+
+function firstLine(x, y) {
+    const res = (-3 / 5) * x + 3;
+    console.log(res);
+console.log();
+
+  return y > (-3 / 5) * x + 3;
+}
+
+function secondLine(x, y) {
+    const res = (2 / 5) * x - 2;
+    console.log(res);
+  return y > (2 / 5) * x - 2;
+}
+
+function thirdLine(x, y) {
+    const res = (-12/8) * x - 12;
+    console.log(res);
+    return y > (-12/8) * x - 12
+}
+
+function forthLine(x, y) {
+    const res = (4 / 7) * x + 4;
+    console.log(res);
+    return y > (4 / 7) * x + 4
+}
--- tests/unit/timeCorectness.spec.js
+++ tests/unit/timeCorectness.spec.js
@@ -1,16 +1,28 @@
-const HomeTask3 = require('../../src/task3');
+const HomeTask3 = require("../../src/task3");
 
-describe('Test', () => {
-    it('first', () => {
-        expect(3).toEqual(3)
-    })
+describe("Test", () => {
+  it("first", () => {
+    expect(3).toEqual(3);
+  });
 
-    it('isTimeValid', () => {
-        expect(HomeTask3.isTimeValide(12, 12)).not.toBeTruthy();
-        expect(HomeTask3.isTimeValide(13, 12)).not.toBeTruthy();
-    })
+//   it("isTimeValid", () => {
+//     expect(HomeTask3.isTimeValide(12, 12)).toBeTruthy();
+//     expect(HomeTask3.isTimeValide(13, 12)).not.toBeTruthy();
+//   });
 
-    it('', () => {
-        
-    })
+//   it("addMinutes", () => {
+//     expect(HomeTask3.addMinutes(12, 25, 30)).toEqual("12:55");
+//     expect(HomeTask3.addMinutes(23, 40, 83)).toEqual("01:03");
+//   });
+
+  it("isPointInCircle", () => {
+    expect(HomeTask3.isPointInCircle(3, 5)).toBeTruthy();
+    expect(HomeTask3.isPointInCircle(3, 1)).toBeTruthy();
+    expect(HomeTask3.isPointInCircle(0, 0)).not.toBeTruthy();
+  });
+
+  it("isPointInSquare", () => {
+      expect(HomeTask3.isPointInSquare(-8, 0)).not.toBeTruthy();
+      expect(HomeTask3.isPointInSquare(1, 1)).toBeTruthy();
+  });
 });
